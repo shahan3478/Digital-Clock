b2a3e02e6697c83022ccac3a3a7b260137c60b2f
shahan-kp <shahan.ahmad@kalpayfinancials.com>
2025-12-22 14:05:55 +0500

    My initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function showTime(){
+    var date=new Date()
+    var h=date.getHours();
+    var m=date.getMinutes();
+    var s=date.getSeconds();
+    var session="PM"
+    if(h==0){
+        h=12
+     }
+     else(h>12)
+        session="AM"
+     
+   //   h=(h<10) ? "0" + h:h;
+   //   m=(h<10) ? "0" + m:m;
+   //   s=(h<10) ? "0" + s:s;
+     var time=h + ":" +  m + ":" + s + session;
+     document.getElementById("Myclockdisplay").innerText=time;
+     setTimeout(showTime,1000);
+    }
+    showTime();
